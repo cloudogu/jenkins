3f8392da2f2491bf79f0a28684f8653ffcf82993
Christoph Wolfes <christoph.wolfes@triology.de>
2018-02-21 16:45:52 +0100

    #6647 adds integration tests for jenkins permission handling

--- integrationTests/cas-browser.spec.js
+++ integrationTests/cas-browser.spec.js
@@ -1,8 +1,11 @@
 const config = require('./config');
 const utils = require('./utils');
-
 const webdriver = require('selenium-webdriver');
 const By = webdriver.By;
+
+const logoutUrl = '/cas/logout';
+const loginUrl = '/cas/login';
+
 
 jest.setTimeout(30000);
 
@@ -16,27 +19,74 @@ afterEach(() => {
     driver.quit();
 });
 
-describe('cas login', () => {
-    test('redirect to cas', async () => {
+describe('cas browser login', () => {
+
+    test('automatic redirect to cas login', async () => {
         driver.get(config.baseUrl + config.jenkinsContextPath);
         const url = await driver.getCurrentUrl();
-        expect(url).toMatch('/cas/login');
+        expect(url).toMatch(loginUrl);
     });
 
-    test('cas authentication', async() => {
+    test('login', async() => {
         driver.get(utils.getCasUrl(driver));
         utils.login(driver);
         const username = await driver.findElement(By.className('login')).getText();
         expect(username).toContain(config.username);
     });
 
-    test('cas logout', async() => {
+    test('logout front channel', async() => {
         driver.get(utils.getCasUrl(driver));
         utils.login(driver);
         await driver.findElement(By.xpath("//div[@id='header']/div[2]/span/a[2]/b")).click();
         const url = await driver.getCurrentUrl();
-        expect(url).toMatch("/cas/logout");
+        expect(url).toMatch(logoutUrl);
+    });
+
+    test('logout back channel', async() => {
+        driver.get(utils.getCasUrl(driver));
+        utils.login(driver);
+        driver.get(config.baseUrl + logoutUrl);
+        driver.get(config.baseUrl + config.jenkinsContextPath);
+        const url = await driver.getCurrentUrl();
+        expect(url).toMatch(loginUrl);
+    });
+
+});
+
+
+describe('browser attributes', () => {
+
+    test('front channel user attributes', async () => {
+        driver.get(utils.getCasUrl(driver));
+        utils.login(driver);
+        await driver.findElement(By.xpath("//div[@id='header']/div[2]/span/a/b")).click();
+        await driver.findElement(By.linkText("Configure")).click();
+        const emailAddressInput = await driver.findElement(By.name("email.address"));
+        const emailAddress = await emailAddressInput.getAttribute("value");
+        const usernameInput= await driver.findElement(By.name('_.fullName'));
+        const username = await usernameInput.getAttribute("value");
+        expect(username).toBe(config.username);
+        expect(emailAddress).toBe(config.email);
+    });
+
+    test('front channel user administrator', async () => {
+        driver.get(utils.getCasUrl(driver));
+        utils.login(driver);
+        var isAdministrator = await utils.isAdministrator(driver);
+        expect(isAdministrator).toBe(true);
+    });
+
+    test('front channel user administrator', async () => {
+        driver.get(utils.getCasUrl(driver));
+        utils.login(driver);
+        var isAdministrator = await utils.isAdministrator(driver);
+        expect(isAdministrator).toBe(true);
     });
 
 
+
 });
+
+
+
+
