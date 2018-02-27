219fb11e69e1b1726708142a3046382ff0d63e03
Christoph Wolfes <christoph.wolfes@triology.de>
2018-02-27 16:55:47 +0100

    #6647 adds wait for element before click

--- integrationTests/adminFunctions.js
+++ integrationTests/adminFunctions.js
@@ -41,9 +41,10 @@ module.exports = class AdminFunctions{
         await utils.getCasUrl(driver);
         await utils.login(driver);
         await driver.get(config.baseUrl + config.jenkinsContextPath + "/asynchPeople");
+        await driver.wait(until.elementLocated(By.linkText(this.testuserName)),5000);
         await driver.findElement(By.linkText(this.testuserName)).click();
         await driver.get(config.baseUrl + config.jenkinsContextPath + "/user/" + this.testuserName + "/delete");
-        await driver.findElement(By.id("yui-gen2-button")).click();};
+        await driver.findElement(By.id("yui-gen3-button")).click();};
 
     async giveAdminRights(){
 
