44113fdc31ba800d650f3cacdad453c842e5d861
Christoph Wolfes <christoph.wolfes@triology.de>
2020-04-16 11:53:42 +0200

    #51 auto installation of java 8
    automatically installs and configures the alpine openjdk version `8.242.08-r0`
    in jenkins.
    
    The installation can be visited at
    
     <Jenkins-URL>/configureTools/ -> JDK

---
+++ resources/var/tmp/resources/init.groovy.d/scripts/init100java8autoinstall.groovy
@@ -0,0 +1,16 @@
+import jenkins.model.*
+import hudson.model.*
+
+def jenkinsJDKName = "openJDK-8"
+def jdkVersion = "java-1.8-openjdk"
+def inst = Jenkins.getInstance()
+
+def desc = inst.getDescriptor("hudson.model.JDK")
+
+JDK jdk = new JDK(jenkinsJDKName, "/usr/lib/jvm/$jdkVersion")
+
+desc.setInstallations(jdk)
+
+desc.save();
+inst.save()
+println("$jdkVersion successfully installed.")
