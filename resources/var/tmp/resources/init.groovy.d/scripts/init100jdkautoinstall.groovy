f3d35847b80170d7c95c4e7b608a02a0aa44011e
Christoph Wolfes <christoph.wolfes@triology.de>
2020-08-19 14:51:43 +0200

    append already installed jdks and fixes some comments

--- resources/var/tmp/resources/init.groovy.d/scripts/init100jdkautoinstall.groovy
+++ resources/var/tmp/resources/init.groovy.d/scripts/init100jdkautoinstall.groovy
@@ -3,13 +3,11 @@ import hudson.model.*
 import hudson.tools.*
 
 def inst = Jenkins.getInstance();
-//def jdkks = inst.getJDKs()
 def desc = inst.getDescriptor("hudson.model.JDK");
 def installedJDKs = JDK[];
 installedJDKs = desc.getInstallations();
 print "init100jdkautoinstall: start jdk installation\n"
 
-// defined jdk installations
 def JDK_8_NAME = "OpenJDK-8";
 // add more jdk-entries to the map to install multiple jdks by default
 def requestedJDKVersions = [ (JDK_8_NAME): "java-1.8-openjdk"];
@@ -17,7 +15,11 @@ def requestedJDKVersions = [ (JDK_8_NAME): "java-1.8-openjdk"];
 // the installations consists of all already installed jdks and all jdks definied in requestedJDKVersions
 def installations = [];
 
-// iterate over all already installed jdks
+for (jdk in requestedJDKVersions) {
+    def installation = new JDK(jdk.key, "/usr/lib/jvm/${jdk.value}")
+    installations.push(installation)
+}
+
 for (jdk in installedJDKs){
     print("init100jdkautoinstall: found installation ${jdk.getName()}\n")
     // do not add requestedJDKVersions
@@ -26,12 +28,6 @@ for (jdk in installedJDKs){
     }else{
         installations.push(jdk)
     }
-}
-
-// iterate over all requested jdk versions
-for (jdk in requestedJDKVersions) {
-    def installation = new JDK(jdk.key, "/usr/lib/jvm/${jdk.value}")
-    installations.push(installation)
 }
 
 // add all installations and save them
