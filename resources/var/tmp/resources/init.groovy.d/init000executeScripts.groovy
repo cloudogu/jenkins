7cce2d110d4076311574ed8c3368ae076ebaeee4
Christoph Wolfes <christoph.wolfes@triology.de>
2020-10-14 15:22:13 +0200

    #68 introduces critical script identification mechanism
    To mark a script as system critical add the prefix *CRIT* after the script number.
    
    To mark a script as non-critical it is recommended to add the prefix *NORM* even though it has no programmatic effect, it results in a better overview of all scripts.
    
    Note: Errors in critical scripts will result in a jenkins shutdown.

--- resources/var/tmp/resources/init.groovy.d/init000executeScripts.groovy
+++ resources/var/tmp/resources/init.groovy.d/init000executeScripts.groovy
@@ -1,5 +1,6 @@
+import groovy.transform.Field
 import groovy.io.FileType
-import jenkins.model.*;
+import jenkins.model.*
 
 /**
  * javadoc on the used classes can be found here:
@@ -7,26 +8,45 @@ import jenkins.model.*;
  */
 
 // Stop Jenkins in case an exception occurs in one of the scripts
-try {
 
+// Mark critical scripts: init010CRITinitScript.groovy
+// When an error in that scripts occur the jenkins will not be startet.
+@Field String CRITICAL_SCRIPT_INDICATOR = "CRIT"
+
+
+try {
 	// Get all filenames from scripts directory
 	def scripts = []
 	def scriptsDirectory = new File(getClass().protectionDomain.codeSource.location.path).parent + "/scripts"
 	new File(scriptsDirectory).eachFile (FileType.FILES) { script ->
 		scripts << script
 	}
-	scripts.sort();
+	scripts.sort()
 
 	// Execute scripts
 	scripts.each {
-		println "Executing post-initialization script ${it}"
-		evaluate(new File("${it}"))
+		scriptPath = it
+		println "Executing post-initialization script ${scriptPath}"
+		evaluate(scriptPath)
 	}
 
 // Stop Jenkins in case an exception occurs
 } catch (Exception exception){
-	println("An exception occured during initialization");
-	exception.printStackTrace();
-	println("Init script and Jenkins exit now...");
-	Jenkins.instance.doExit(null, null);
+	println "An exception occured during initialization"
+	exception.printStackTrace()
+	filename = scriptPath.name
+	if (isCriticalScript(filename)){
+		println "Critical init script ${filename} returned an error. Shutting down jenkins..."
+		Jenkins.instance.doExit(null, null);
+	}else{
+		println "Non-critical init script ${filename} returned an error. Continuing jenkins startup..."
+	} 
+}
+
+def isCriticalScript(String name){
+	if (name.contains(CRITICAL_SCRIPT_INDICATOR)){
+		return true
+	}else{
+		return false
+	}
 }
