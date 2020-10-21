ae040defcdc4766932813ff84eedfbb03eeda778
Robert Auer <robert.auer@cloudogu.com>
2020-10-21 15:36:33 +0200

    Continue executing scripts if a normal script fails; #68

--- resources/var/tmp/resources/init.groovy.d/init000executeScripts.groovy
+++ resources/var/tmp/resources/init.groovy.d/init000executeScripts.groovy
@@ -1,5 +1,6 @@
 import groovy.transform.Field
 import groovy.io.FileType
+
 import jenkins.model.*
 
 /**
@@ -11,38 +12,35 @@ import jenkins.model.*
 
 // Mark critical scripts, for example: init010CRITinitScript.groovy
 // If an error occurs in a critical script, Jenkins will not be started.
-@Field String CRITICAL_SCRIPT_INDICATOR = "CRIT"
+@Field String CRITICAL_SCRIPT_INDICATOR = 'CRIT'
 
+// Get all filenames from scripts directory
+def scripts = []
+def scriptsDirectory = new File(getClass().protectionDomain.codeSource.location.path).parent + '/scripts'
+new File(scriptsDirectory).eachFile (FileType.FILES) { script ->
+    scripts << script
+}
+scripts.sort()
 
-try {
-	// Get all filenames from scripts directory
-	def scripts = []
-	def scriptsDirectory = new File(getClass().protectionDomain.codeSource.location.path).parent + "/scripts"
-	new File(scriptsDirectory).eachFile (FileType.FILES) { script ->
-		scripts << script
-	}
-	scripts.sort()
-
-	// Execute scripts
-	scripts.each {
-		scriptPath = it
-		println "Executing post-initialization script ${scriptPath}"
-		evaluate(scriptPath)
-	}
-
-// Stop Jenkins in case an exception occurs
-} catch (Exception exception){
-	println "An exception occured during initialization"
-	exception.printStackTrace()
-	filename = scriptPath.name
-	if (isCriticalScript(filename)){
-		println "Critical init script ${filename} returned an error. Shutting down jenkins..."
-		Jenkins.instance.doExit(null, null);
-	} else {
-		println "Non-critical init script ${filename} returned an error. Continuing jenkins startup..."
-	} 
+// Execute scripts
+scripts.each {
+    scriptPath = it
+    println "Executing post-initialization script ${scriptPath}"
+    try {
+        evaluate(scriptPath)
+	} catch (Exception exception) {
+        println 'An exception occured during initialization'
+        exception.printStackTrace()
+        filename = scriptPath.name
+        if (isCriticalScript(filename)) {
+            println "Critical init script ${filename} returned an error. Shutting down jenkins..."
+            Jenkins.instance.doExit(null, null)
+    	} else {
+            println "Non-critical init script ${filename} returned an error. Continuing jenkins startup..."
+        }
+    }
 }
 
-def isCriticalScript(String name){
-	return name.contains(CRITICAL_SCRIPT_INDICATOR)
+boolean isCriticalScript(String name) {
+    return name.contains(CRITICAL_SCRIPT_INDICATOR)
 }
