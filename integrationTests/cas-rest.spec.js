5011b187c3c3af42eb52f29595cda79ca168be4d
Joshua Sprey <joshua.sprey@cloudogu.com>
2021-05-18 15:38:27 +0200

    Fixes warp menu blocking the login process in the integration tests for Jenkins

--- integrationTests/cas-rest.spec.js
+++ integrationTests/cas-rest.spec.js
@@ -20,12 +20,9 @@ describe('cas rest basic authentication', () => {
 
     test('authentication with API key', async () => {
         driver = utils.createDriver(webdriver);
-        // hide warp menu hint
         await driver.get(utils.getCasUrl(driver));
-        await driver.manage().addCookie({
-            name: "warpMenuHideTooltip",
-            value: "hide"
-        });
+        // hide warp menu hint
+        driver.getLocalStorage().setItem("warpMenuHideTooltip", "hide");
         await driver.get(utils.getCasUrl(driver));
         await utils.login(driver);
         // go to user configuration page
