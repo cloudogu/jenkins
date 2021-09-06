d95769bbce8dbd2dd9d6fb516978c4dbe1978d4e
Philipp Pixel <boris.schaa@cloudogu.com>
2021-09-06 14:39:53 +0200

    #94 Secure main startup code with source guard
    This is a safety measure in order to allow the existing code both
    to run as well as to be tested.

--- resources/startup.sh
+++ resources/startup.sh
@@ -3,84 +3,90 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-INIT_SCRIPT_FOLDER="/var/lib/jenkins/init.groovy.d"
-# TODO rename resources to jenkins
-MAIN_INIT_SCRIPTS_FOLDER="/var/tmp/resources/init.groovy.d"
-CUSTOM_INIT_SCRIPTS_FOLDER="/var/lib/custom.init.groovy.d"
+run_main() {
+  INIT_SCRIPT_FOLDER="/var/lib/jenkins/init.groovy.d"
+  # TODO rename resources to jenkins
+  MAIN_INIT_SCRIPTS_FOLDER="/var/tmp/resources/init.groovy.d"
+  CUSTOM_INIT_SCRIPTS_FOLDER="/var/lib/custom.init.groovy.d"
 
-# set state to installing
-doguctl state 'installing'
+  # set state to installing
+  doguctl state 'installing'
 
-# create truststore for java processes
-TRUSTSTORE="/var/lib/jenkins/truststore.jks"
-create_truststore.sh /var/lib/jenkins/truststore.jks > /dev/null
+  # create truststore for java processes
+  TRUSTSTORE="/var/lib/jenkins/truststore.jks"
+  create_truststore.sh /var/lib/jenkins/truststore.jks > /dev/null
 
-# create ca store for git, mercurial and subversion
-create-ca-certificates.sh /var/lib/jenkins/ca-certificates.crt
+  # create ca store for git, mercurial and subversion
+  create-ca-certificates.sh /var/lib/jenkins/ca-certificates.crt
 
-# copy init scripts
+  # copy init scripts
 
-# remove old folder to be sure,
-# that it contains no script which is already removed from custom init script folder
-if [ -d "${INIT_SCRIPT_FOLDER}" ]; then
-  rm -rf "${INIT_SCRIPT_FOLDER}"
-fi
-
-# copy fresh main init scripts
-cp -rf "${MAIN_INIT_SCRIPTS_FOLDER}" "${INIT_SCRIPT_FOLDER}"
-
-# merge custom init scripts, if the volume is not empty
-if [ "$(ls -A ${CUSTOM_INIT_SCRIPTS_FOLDER})" ]; then
-  cp "${CUSTOM_INIT_SCRIPTS_FOLDER}"/* "${INIT_SCRIPT_FOLDER}"/scripts
-fi
-
-# set initial setting for slave-to-master-security
-# see https://wiki.jenkins-ci.org/display/JENKINS/Slave+To+Master+Access+Control
-SLAVE_TO_MASTER_SECURITY="/var/lib/jenkins/secrets/slave-to-master-security-kill-switch"
-if [ ! -f "${SLAVE_TO_MASTER_SECURITY}" ]; then
-  SECRETS_DIRECTORY=$(dirname "${SLAVE_TO_MASTER_SECURITY}")
-  if [ ! -d "${SECRETS_DIRECTORY}" ]; then
-    mkdir -p "${SECRETS_DIRECTORY}"
+  # remove old folder to be sure,
+  # that it contains no script which is already removed from custom init script folder
+  if [ -d "${INIT_SCRIPT_FOLDER}" ]; then
+    rm -rf "${INIT_SCRIPT_FOLDER}"
   fi
-  echo 'false' > "${SLAVE_TO_MASTER_SECURITY}"
-fi
 
-# Disable CLI over Remoting as advised with Jenkins LTS 2.46.2
-# see https://jenkins.io/blog/2017/04/26/security-advisory/
-CLI_CONFIG_FILE="/var/lib/jenkins/jenkins.CLI.xml"
-if [ ! -f "${CLI_CONFIG_FILE}" ]; then
-	cp /var/tmp/resources/jenkins.CLI.xml "${CLI_CONFIG_FILE}"
-	chmod 0644 "${CLI_CONFIG_FILE}"
-fi
+  # copy fresh main init scripts
+  cp -rf "${MAIN_INIT_SCRIPTS_FOLDER}" "${INIT_SCRIPT_FOLDER}"
 
-# Set maven truststore options in .mavenrc file so they won't get copied to slave machines
-if [[ ! -e /var/lib/jenkins/.mavenrc ]]; then
-  echo "MAVEN_OPTS=\"\$MAVEN_OPTS -Djavax.net.ssl.trustStore=${TRUSTSTORE}\"" > /var/lib/jenkins/.mavenrc
-  echo "MAVEN_OPTS=\"\$MAVEN_OPTS -Djavax.net.ssl.trustStorePassword=changeit\"" >> /var/lib/jenkins/.mavenrc
-fi
+  # merge custom init scripts, if the volume is not empty
+  if [ "$(ls -A ${CUSTOM_INIT_SCRIPTS_FOLDER})" ]; then
+    cp "${CUSTOM_INIT_SCRIPTS_FOLDER}"/* "${INIT_SCRIPT_FOLDER}"/scripts
+  fi
 
-# starting jenkins
-if [[ "$(doguctl config "container_config/memory_limit" -d "empty")" == "empty" ]];  then
-  echo "Starting Jenkins without memory limits..."
-  java -Djava.awt.headless=true \
-    -Djava.net.preferIPv4Stack=true \
-    -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
-    -Djavax.net.ssl.trustStorePassword=changeit \
-    -Djenkins.install.runSetupWizard=false \
-    -Djava.awt.headless=true \
-    -jar /jenkins.war --prefix=/jenkins
-else
-  # Retrieve configurable java limits from etcd, valid default values exist
-  MEMORY_LIMIT_MAX_PERCENTAGE=$(doguctl config "container_config/java_max_ram_percentage")
-  MEMORY_LIMIT_MIN_PERCENTAGE=$(doguctl config "container_config/java_min_ram_percentage")
-  echo "Starting Jenkins with memory limits: MaxRAMPercentage=${MEMORY_LIMIT_MAX_PERCENTAGE}, MinRAMPercentage=${MEMORY_LIMIT_MIN_PERCENTAGE} ..."
-  java -Djava.awt.headless=true \
-    -Djava.net.preferIPv4Stack=true \
-    -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
-    -Djavax.net.ssl.trustStorePassword=changeit \
-    -Djenkins.install.runSetupWizard=false \
-    -Djava.awt.headless=true \
-    -XX:MaxRAMPercentage="${MEMORY_LIMIT_MAX_PERCENTAGE}" \
-    -XX:MinRAMPercentage="${MEMORY_LIMIT_MIN_PERCENTAGE}" \
-    -jar /jenkins.war --prefix=/jenkins
+  # set initial setting for slave-to-master-security
+  # see https://wiki.jenkins-ci.org/display/JENKINS/Slave+To+Master+Access+Control
+  SLAVE_TO_MASTER_SECURITY="/var/lib/jenkins/secrets/slave-to-master-security-kill-switch"
+  if [ ! -f "${SLAVE_TO_MASTER_SECURITY}" ]; then
+    SECRETS_DIRECTORY=$(dirname "${SLAVE_TO_MASTER_SECURITY}")
+    if [ ! -d "${SECRETS_DIRECTORY}" ]; then
+      mkdir -p "${SECRETS_DIRECTORY}"
+    fi
+    echo 'false' > "${SLAVE_TO_MASTER_SECURITY}"
+  fi
+
+  # Disable CLI over Remoting as advised with Jenkins LTS 2.46.2
+  # see https://jenkins.io/blog/2017/04/26/security-advisory/
+  CLI_CONFIG_FILE="/var/lib/jenkins/jenkins.CLI.xml"
+  if [ ! -f "${CLI_CONFIG_FILE}" ]; then
+    cp /var/tmp/resources/jenkins.CLI.xml "${CLI_CONFIG_FILE}"
+    chmod 0644 "${CLI_CONFIG_FILE}"
+  fi
+
+  # Set maven truststore options in .mavenrc file so they won't get copied to slave machines
+  if [[ ! -e /var/lib/jenkins/.mavenrc ]]; then
+    echo "MAVEN_OPTS=\"\$MAVEN_OPTS -Djavax.net.ssl.trustStore=${TRUSTSTORE}\"" > /var/lib/jenkins/.mavenrc
+    echo "MAVEN_OPTS=\"\$MAVEN_OPTS -Djavax.net.ssl.trustStorePassword=changeit\"" >> /var/lib/jenkins/.mavenrc
+  fi
+
+  # starting jenkins
+  if [[ "$(doguctl config "container_config/memory_limit" -d "empty")" == "empty" ]];  then
+    echo "Starting Jenkins without memory limits..."
+    java -Djava.awt.headless=true \
+      -Djava.net.preferIPv4Stack=true \
+      -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
+      -Djavax.net.ssl.trustStorePassword=changeit \
+      -Djenkins.install.runSetupWizard=false \
+      -Djava.awt.headless=true \
+      -jar /jenkins.war --prefix=/jenkins
+  else
+    # Retrieve configurable java limits from etcd, valid default values exist
+    MEMORY_LIMIT_MAX_PERCENTAGE=$(doguctl config "container_config/java_max_ram_percentage")
+    MEMORY_LIMIT_MIN_PERCENTAGE=$(doguctl config "container_config/java_min_ram_percentage")
+    echo "Starting Jenkins with memory limits: MaxRAMPercentage=${MEMORY_LIMIT_MAX_PERCENTAGE}, MinRAMPercentage=${MEMORY_LIMIT_MIN_PERCENTAGE} ..."
+    java -Djava.awt.headless=true \
+      -Djava.net.preferIPv4Stack=true \
+      -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
+      -Djavax.net.ssl.trustStorePassword=changeit \
+      -Djenkins.install.runSetupWizard=false \
+      -Djava.awt.headless=true \
+      -XX:MaxRAMPercentage="${MEMORY_LIMIT_MAX_PERCENTAGE}" \
+      -XX:MinRAMPercentage="${MEMORY_LIMIT_MIN_PERCENTAGE}" \
+      -jar /jenkins.war --prefix=/jenkins
+  fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  run_main
 fi
