6dee6753205c0a91814cabeb9abfe4451d3bca8d
Philipp Pixel <boris.schaa@cloudogu.com>
2021-09-10 14:42:16 +0200

    #94 Bootstrap BATS shell tests

---
+++ unitTests/usr/bin/create-ca-certificates.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# this is a mock script being sourced from a production script.
+# the actual script origins from the base image
+
+ADDITIONAL_CERTIFICATES_DIR_KEY="certificate/additional"
+ADDITIONAL_CERTIFICATES_TOC="${ADDITIONAL_CERTIFICATES_DIR_KEY}/toc"
+
+function existAdditionalCertificates() {
+  additionalCertTOC="$(doguctl config  --default "" --global "${ADDITIONAL_CERTIFICATES_TOC}")"
+
+  if [[ -z "${additionalCertTOC// }" ]] ; then
+    return 1
+  else
+    return 0
+  fi
+}
