d1fceac049a0bd3ecfc5d8abadabbb15c05de2c8
Simon Klein <simon.klein-extern@cloudogu.com>
2023-07-06 14:05:47 +0200

    #129 Fix args at startup

--- resources/startup.sh
+++ resources/startup.sh
@@ -76,22 +76,25 @@ run_main() {
     echo "MAVEN_OPTS=\"\$MAVEN_OPTS -Djavax.net.ssl.trustStorePassword=changeit\"" >> "${JENKINS_HOME_DIR}/.mavenrc"
   fi
 
-  DEFAULT_JENKINS_ARGS="-Djava.awt.headless=true -Djavax.net.ssl.trustStore=${TRUSTSTORE} -Djavax.net.ssl.trustStorePassword=changeit -Djenkins.install.runSetupWizard=false -Djava.awt.headless=true"
+  JENKINS_ARGS="-Djava.awt.headless=true -Djavax.net.ssl.trustStore=${TRUSTSTORE} -Djavax.net.ssl.trustStorePassword=changeit -Djenkins.install.runSetupWizard=false"
   ADDITIONAL_JENKINS_ARGS="$(doguctl config additional_java_args)"
+  if [[ "${ADDITIONAL_JENKINS_ARGS}" != "UNSET" ]];
+  then
+    JENKINS_ARGS="${JENKINS_ARGS} ${ADDITIONAL_JENKINS_ARGS}"
+  fi
 
   # starting jenkins
   if [[ "$(doguctl config "container_config/memory_limit" -d "empty")" == "empty" ]];  then
     echo "Starting Jenkins without memory limits..."
-    JENKINS_ARGS="${DEFAULT_JENKINS_ARGS} ${ADDITIONAL_JENKINS_ARGS}"
   else
     # Retrieve configurable java limits from etcd, valid default values exist
     MEMORY_LIMIT_MAX_PERCENTAGE=$(doguctl config "container_config/java_max_ram_percentage")
     MEMORY_LIMIT_MIN_PERCENTAGE=$(doguctl config "container_config/java_min_ram_percentage")
     echo "Starting Jenkins with memory limits: MaxRAMPercentage=${MEMORY_LIMIT_MAX_PERCENTAGE}, MinRAMPercentage=${MEMORY_LIMIT_MIN_PERCENTAGE} ..."
-    JENKINS_ARGS="${DEFAULT_JENKINS_ARGS} ${ADDITIONAL_JENKINS_ARGS} -XX:MaxRAMPercentage=${MEMORY_LIMIT_MAX_PERCENTAGE} -XX:MinRAMPercentage=${MEMORY_LIMIT_MIN_PERCENTAGE}"
+    JENKINS_ARGS="${JENKINS_ARGS} -XX:MaxRAMPercentage=${MEMORY_LIMIT_MAX_PERCENTAGE} -XX:MinRAMPercentage=${MEMORY_LIMIT_MIN_PERCENTAGE}"
   fi
 
-  java "${JENKINS_ARGS}" -jar /jenkins.war --prefix=/jenkins
+  java ${JENKINS_ARGS} -jar /jenkins.war --prefix=/jenkins
 }
 
 function createCurlCertificates() {
