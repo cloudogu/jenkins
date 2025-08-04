f4843a622800aabde7483dc7cca5ba49c69424b3
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2025-08-04 11:46:41 +0200

    #223 add new token for cypress test

--- integrationTests/cypress/support/commands/jenkins_gui.js
+++ integrationTests/cypress/support/commands/jenkins_gui.js
@@ -23,7 +23,7 @@ const createJenkinsApiKey = () => {
     })
     cy.get("button").contains("Add new token").click({force: true});
     cy.get("button").contains("Generate").click({force: true});
-    return cy.get("span[class='new-token-value visible']").invoke("text");
+    return cy.get("span[class='api-token-new-value']").invoke("text");
 };
 
 const getUserAttributesGui = (testUser) => {
