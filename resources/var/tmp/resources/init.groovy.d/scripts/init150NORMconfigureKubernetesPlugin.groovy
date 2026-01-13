5b5b2d1d32473d44c5e3506a90a5d0413b9214b8
Jeremias Weber <jeremias.weber@cloudogu.com>
2026-01-13 16:39:40 +0100

    #241 Add configuration options for kubernetes agents

--- resources/var/tmp/resources/init.groovy.d/scripts/init150NORMconfigureKubernetesPlugin.groovy
+++ resources/var/tmp/resources/init.groovy.d/scripts/init150NORMconfigureKubernetesPlugin.groovy
@@ -2,6 +2,7 @@ package scripts
 
 import jenkins.model.*
 import org.csanchez.jenkins.plugins.kubernetes.*
+import groovy.json.JsonSlurper;
 
 def jenkins = Jenkins.instance
 
@@ -14,17 +15,42 @@ def getDoguctlWrapper() {
 
 doguctl = getDoguctlWrapper()
 
-if (doguctl.isMultinode()) {
-    def kubernetesCloud = new KubernetesCloud("kubernetes")
+def kubernetesCloud = new KubernetesCloud("kubernetes")
+if (doguctl.isMultinode() && doguctl.getDoguConfig("enable_kubernetes_agents") == "true") {
 
-    kubernetesCloud.setServerUrl("kubernetes.default.svc.cluster.local")
+    kubernetesCloud.setServerUrl("https://kubernetes.default.svc.cluster.local")
     def crt = new File("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt").text
     kubernetesCloud.setServerCertificate(crt)
-    kubernetesCloud.setNamespace("jenkins-build")
-    kubernetesCloud.setPodLabels([new PodLabel("cloudogu.com/pod-kind", "jenkins-build")])
-    kubernetesCloud.setJenkinsUrl("http://jenkins.ecosystem.svc.cluster.local:8080/jenkins")
+
+    def agentNamespace = doguctl.getDoguConfig("agent_kubernetes_namespace")
+    kubernetesCloud.setNamespace(agentNamespace)
+
+    def podLabelsJson = doguctl.getDoguConfig("agent_kubernetes_pod_labels")
+    def jsonSlurper = new JsonSlurper()
+    def podLabelsMap = jsonSlurper.parseText(podLabelsJson)
+    def podLabels = podLabelsMap.collectEntries{key, value -> [new PodLabel(key, value)]}
+    kubernetesCloud.setPodLabels(podLabels)
+
+    def restrictedSecurityContext = doguctl.getDoguConfig("agent_kubernetes_restricted_pss_security_context")
+    kubernetesCloud.setRestrictedPssSecurityContext(restrictedSecurityContext == "true")
+
+    def agentImageRegistry = doguctl.getDoguConfig("agent_kubernetes_docker_registry")
+    kubernetesCloud.setJnlpregistry(agentImageRegistry)
+
+    def enableGarbageCollection = doguctl.getDoguConfig("agent_kubernetes_enable_garbage_collection")
+    if (enableGarbageCollection == "true") {
+        def garbageCollection = new GarbageCollection()
+        garbageCollection.setTimeout(300)
+        kubernetesCloud.setGarbageCollection(garbageCollection)
+    }
+
+    def ecosystemNamespace = doguctl.getDoguConfig("ecosystem_kubernetes_namespace")
+    kubernetesCloud.setJenkinsUrl("http://jenkins.${ecosystemNamespace}.svc.cluster.local:8080/jenkins")
     kubernetesCloud.setWebSocket(true)
 
     jenkins.clouds.replace(kubernetesCloud)
-    jenkins.save()
+} else {
+    jenkins.clouds.remove(kubernetesCloud.getDescriptor())
 }
+
+jenkins.save()
