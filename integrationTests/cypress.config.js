8a767e86c7f4adb437d0f2b4925c44acaedf2634
Marco Bergen <marco.bergen-extern@cloudogu.com>
2026-02-09 14:30:29 +0000

    update makefiles

--- integrationTests/cypress.config.js
+++ integrationTests/cypress.config.js
@@ -21,13 +21,13 @@ async function setupNodeEvents(on, config) {
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://192.168.56.2',
+        baseUrl: "https://34.107.73.133",
         env: {
             "DoguName": "jenkins",
             "MaxLoginRetries": 3,
-            "AdminUsername":  "ces-admin",
-            "AdminPassword":  "Ecosystem2016!",
-            "AdminGroup":  "CesAdministrators"
+            "AdminUsername": "mbergen",
+            "AdminPassword":  "l+kk)GyGI3=}d1Ud",
+            "AdminGroup":  "cesAdmin"
         },
         videoCompression: false,
         specPattern: ["cypress/e2e/**/*.feature"],
