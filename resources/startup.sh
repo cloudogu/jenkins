33762b6cf14252798f011952d868b57758c1e6f1
l-friebe <laurin.friebe-extern@cloudogu.com>
2026-08-05 08:17:30 +0200

    Update resources/startup.sh
    Co-authored-by: Philipp Pixel <ppxl@users.noreply.github.com>

--- resources/startup.sh
+++ resources/startup.sh
@@ -138,6 +138,7 @@ function start_jenkins() {
 
   # disable shellcheck her. This behaviour is intended
   # shellcheck disable=SC2086
+# use JAVA_HOME from the base Java installation. Update the base java version to use a different one.
   "${JAVA_HOME}/bin/java" "${JENKINS_ARGS[@]}" -jar /jenkins.war --prefix=/jenkins
 }
 
